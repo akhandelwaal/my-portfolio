55206207c19ab2b9d3b59ccfabc502129436aa04
Ankit Khandelwal <khandelwaal.ankit@gmail.com>
2018-01-30 00:12:35 +0530

    Used SNS for notification

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,20 +5,30 @@ import zipfile
 #import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_bucket = s3.Bucket('portfolio.ankitkhandelwal.info')
-    build_bucket = s3.Bucket('portfoliobuild.ankitkhandelwal.info')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:175319350514:deployPortfolioTopic')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-#            portfolio_bucket.upload_fileobj(obj, nm,
-#                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_bucket = s3.Bucket('portfolio.ankitkhandelwal.info')
+        build_bucket = s3.Bucket('portfoliobuild.ankitkhandelwal.info')
 
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+    #            portfolio_bucket.upload_fileobj(obj, nm,
+    #                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print 'Job Done!'
+        topic.publish(Subject="Portfolio deployed", Message="Portfolio deployed successfully.")
+    except:
+        topic.publish(Subject="Portfolio deployment failed", Message="The Portfolio was not deployed successfully.")
+        raise
     return 'Deployed successfully'
