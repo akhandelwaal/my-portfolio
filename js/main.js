24357e6b6bbaacc1e9a9389a5bcaba133e56a78b
Ankit Khandelwal <khandelwaal.ankit@gmail.com>
2018-02-12 20:49:03 +0530

    move statuc markup for work examples into react component

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Kredeit Banking Corporation",
+    'image': {
+      'desc': "example screenshot of a project involving code",
+      'src': "images/example1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Duetche Bahn Systel",
+    'image': {
+      'desc': "A Serverless Portfolio",
+      'src': "images/example2.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "SEI Investments",
+    'image': {
+      'desc': "example screenshot of a project involving cats",
+      'src': "images/example3.png",
+      'comment': ""
+    }
+  }
+]
 
-console.log("Webpack Works!");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
